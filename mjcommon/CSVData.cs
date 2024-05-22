2116f8349e8f6be0488cdc3d56dc07753d8c1672
Callum Johnson <callumbro@hotmail.com>
2024-05-22 03:57:35 -0400

    upgrade to .net 8

--- mjcommon/CSVData.cs
+++ mjcommon/CSVData.cs
@@ -1,193 +1,208 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Text;
-using Microsoft.VisualBasic.FileIO;
+using System.Linq;
 
 namespace RaceBeam
 {
-	/// <summary>
-	/// Class to hold data loaded from csv file
-	/// Requires that the csv file have a header line
-	/// Read-only
-	/// </summary>
-	public class CSVData
-	{
-		// We use a dictionary containing other dictionaries
-		// Key is first column in CSV file, it keys to dict of header names and values
-		// Access via data["driver number"]["header name"]
-		
-		private readonly Dictionary<string, Dictionary<string,string>> _data = new Dictionary<string, Dictionary<string, string>>();
-		private List<string> headers = new List<string>();
-		
-		// Return the array of headers
-		public List<string> GetHeaders()
-		{
-			return headers;
-		}
-		// Return a list of keys
-		public List<string> getKeys()
-		{
-			var list = new List<string>(_data.Keys);
-			return list;
-		}
-		
-		/// <summary>
-		/// Gets the field value at the specified record index for the supplied field name
-		/// </summary>
-		/// <param name="key">Car number</param>
-		/// <param name="fieldName">Field name</param>
-		/// <returns>Value string or empty string</returns>
-		public string GetField(string key, string fieldName)
-		{
-			if (key == null)
-			{
-				return "";
-			}
-			if ((_data.ContainsKey(key)) && (_data[key].ContainsKey(fieldName)))
-			{
-				return _data[key][fieldName];
-			}
-			else
-			{
-				return "";
-			}
-		}
-		/// <summary>
-		/// Returns number of keyed elements (lines in CSV file)
-		/// </summary>
-		/// 
-		public int Length()
-		{
-			return _data.Count;
-		}
-		
-		/// <param name="filePath"></param>
-		/// <summary>
-		/// Reads in the driver or timing data from the specified csv file
-		/// Dictionary key is first column heading name
-		/// </summary>
-		/// <param name="filePath">File path</param>
-		/// <param name="separator">character separator</param>
-		/// <param name="keyField">NAme of key field (must be unique)</param>
-		public string LoadData(string filePath, char separator, string keyField)
-		{
-			_data.Clear();
-			bool addedHeader = false;
-			int numColumns;
-			int myKey = 0;	// when we invent a key field
-			try
-			{
-				var file = new System.IO.StreamReader(
-					File.Open(filePath,FileMode.Open,
-						FileAccess.Read,
-						FileShare.ReadWrite));
-				using (TextFieldParser parser = new TextFieldParser(file))
-				{
-					if (separator == ',')
-					{
-						parser.SetDelimiters(",");
-					}
-					else
-					{
-						parser.SetDelimiters("\t");
-					}
-					parser.TextFieldType = FieldType.Delimited;
-					
-					while (!parser.EndOfData)
-					{
-						string[] columns;
-						try
-						{
-							columns = parser.ReadFields();
-						}
-						catch
-						{
-							// not a valid delimited line - log, terminate, or ignore
-							continue;
-						}
-						
-						if ((columns.Length < 3) || ((columns.Length == 3) && (string.IsNullOrEmpty(columns[2]) == true)))
-						{
-							// didn't work -- skip this line
-							continue;
-						}
-						for (int i = 0; i < columns.Length; i++)
-						{
-							columns[i] = columns[i].Replace(",",";");
-							columns[i] = columns[i].Replace("\t"," ");
-						}
-						if (!addedHeader)
-						{
-							headers = new List<string>();
-							numColumns = columns.Length;
-							for (int i = 0; i < columns.Length; i++)
-							{
-								if (string.IsNullOrEmpty(columns[i]) == true)
-								{
-									columns[i] = "col" + i.ToString();
-								}
-								headers.Add(columns[i]);
-								addedHeader = true;
-							}
-							if (keyField == "")
-							{
-								headers.Add("myKey");
-							}
-							continue;
-						}
-					
-						var record = new Dictionary<string, string>();
-					
-						for (int i = 0; i < columns.Length; i++)
-						{
-							if (i >= headers.Count)
-							{
-								continue;
-							}
-							if (string.IsNullOrEmpty(columns[i]) == true)
-							{
-								//columns[i] = "col" + i.ToString();
-							}
-							if (record.ContainsKey(headers[i]))
-							{
-								continue;
-							}
-							record.Add(headers[i], columns[i]);
-						}
-						if (keyField == "")
-						{
-							string nextKey = myKey.ToString();
-							myKey += 1;
-							if (record.ContainsKey(nextKey))
-							{
-								continue;
-							}
-							record.Add("myKey", nextKey);
-						}
-						string key = keyField;
-						if (keyField == "")
-						{
-							key = "myKey";
-						}
-						if (record.ContainsKey(key))
-						{
-							if (_data.ContainsKey(record[key]))
-							{
-								continue;
-							}
-							_data.Add(record[key], record);
-						}
-					}
-				}
-				file.Close();
-				file = null;
-			}
-			catch (Exception e)
-			{
-				return(e.Message);
-			}
-			return "";
-		}
-	}
+    /// <summary>
+    /// Class to hold data loaded from csv file
+    /// Requires that the csv file have a header line
+    /// Read-only
+    /// </summary>
+    public class CSVData
+    {
+        // We use a dictionary containing other dictionaries
+        // Key is first column in CSV file, it keys to dict of header names and values
+        // Access via data["driver number"]["header name"]
+
+        private readonly Dictionary<string, Dictionary<string, string>> _data = new Dictionary<string, Dictionary<string, string>>();
+        private List<string> headers = new List<string>();
+
+        // Return the array of headers
+        public List<string> GetHeaders()
+        {
+            return headers;
+        }
+        // Return a list of keys
+        public List<string> getKeys()
+        {
+            var list = new List<string>(_data.Keys);
+            return list;
+        }
+
+        /// <summary>
+        /// Gets the field value at the specified record index for the supplied field name
+        /// </summary>
+        /// <param name="key">Car number</param>
+        /// <param name="fieldName">Field name</param>
+        /// <returns>Value string or empty string</returns>
+        public string GetField(string key, string fieldName)
+        {
+            if (key == null)
+            {
+                return "";
+            }
+            if ((_data.ContainsKey(key)) && (_data[key].ContainsKey(fieldName)))
+            {
+                return _data[key][fieldName];
+            }
+            else
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Returns number of keyed elements (lines in CSV file)
+        /// </summary>
+        /// 
+        public int Length()
+        {
+            return _data.Count;
+        }
+
+        /// <param name="filePath"></param>
+        /// <summary>
+        /// Reads in the driver or timing data from the specified csv file
+        /// Dictionary key is first column heading name
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        /// <param name="separator">character separator</param>
+        /// <param name="keyField">Name of key field (must be unique)</param>
+        public string LoadData(string filePath, char separator, string keyField)
+        {
+            // Clear out the data for new data.
+            _data.Clear();
+
+            // Variables for generating a unique primary key when not specified.
+            string myKeyHeaderDefault = "myKey";
+            int myKey = 0;  // counter to keep the newly invented keys unique
+
+            try
+            {
+                CsvConfiguration csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
+                if (separator == ',')
+                {
+                    csvConfiguration.Delimiter = ",";
+                }
+                else
+                {
+                    csvConfiguration.Delimiter = "\t";
+                }
+
+                using (StreamReader reader = new StreamReader(filePath))
+                using (CsvReader csvReader = new CsvReader(reader, csvConfiguration))
+                {
+                    // Read in the headers before the data.
+                    csvReader.Read();
+                    csvReader.ReadHeader();
+                    headers = csvReader.HeaderRecord.ToList();
+                    for (int i = 0; i < headers.Count; i++)
+                    {
+                        if (string.IsNullOrWhiteSpace(headers[i]))
+                        {
+                            // Do not allow a column header to be blank.
+                            headers[i] = "col" + i.ToString();
+                        }
+                    }
+                    // Make sure there is a primary key column.
+                    if (string.IsNullOrWhiteSpace(keyField))
+                    {
+                        headers.Add(myKeyHeaderDefault);
+
+                        // Set the keyField to the default.
+                        keyField = myKeyHeaderDefault;
+                    }
+
+                    // Read and parse the data rows.
+                    while (csvReader.Read())
+                    {
+                        List<string> csvRowData = new List<string>();
+                        try
+                        {
+                            // Read in all the data for the row.
+                            for (int i = 0; i < csvReader.ColumnCount; i++)
+                            {
+                                csvRowData.Add(csvReader.GetField(i));
+                            }
+                        }
+                        catch
+                        {
+                            // not a valid delimited line - log, terminate, or ignore
+                            continue;
+                        }
+
+                        if ((csvRowData.Count < 3) || ((csvRowData.Count == 3) && (string.IsNullOrEmpty(csvRowData[2]) == true)))
+                        {
+                            // didn't work -- skip this line
+                            continue;
+                        }
+                        for (int i = 0; i < csvRowData.Count; i++)
+                        {
+                            csvRowData[i] = csvRowData[i].Replace(",", ";");
+                            csvRowData[i] = csvRowData[i].Replace("\t", " ");
+                        }
+
+                        var record = new Dictionary<string, string>();
+
+                        for (int i = 0; i < csvRowData.Count; i++)
+                        {
+                            if (i >= headers.Count)
+                            {
+                                // Somehow there was more data than headers.
+                                // Skip as we do not know what the data represents.
+                                break;
+                            }
+                            if (record.ContainsKey(headers[i]))
+                            {
+                                // Record already has data for this column.
+                                continue;
+                            }
+
+                            // Add the data to the record.
+                            record.Add(headers[i], csvRowData[i]);
+                        }
+
+                        if (keyField == myKeyHeaderDefault)
+                        {
+                            // Determine a unique primary key.
+                            string nextKey = myKey.ToString();
+                            myKey += 1;
+                            if (record.ContainsKey(nextKey))
+                            {
+                                // There is alreay data for this key, skip to next csv data line.
+                                continue;
+                            }
+
+                            // Add the primary key to the record.
+                            record.Add("myKey", nextKey);
+                        }
+
+                        if (record.ContainsKey(keyField))
+                        {
+                            // The record as a key, so it can be saved to _data.
+
+                            if (_data.ContainsKey(record[keyField]))
+                            {
+                                // This data has already been saved, don't overwrite it.
+                                continue;
+                            }
+
+                            // Add the new record.
+                            _data.Add(record[keyField], record);
+                        }
+                    }
+                }
+
+                return "";
+            }
+            catch (Exception e)
+            {
+                return (e.Message);
+            }
+        }
+    }
 }
